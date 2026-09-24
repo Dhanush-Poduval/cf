0b19d98028df4c931b20c9c82e87b512c011f91f
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-09-24 19:22:27 +0530

    brute force 2

--- eight00/subarrays_length.cpp
+++ eight00/subarrays_length.cpp
@@ -11,27 +11,32 @@ int main() {
     cin >> b;
     string s;
     cin >> s;
-
+    int count = 0;
     vector<string> sub_arr;
     for (int k = 0; k < b; k++) {
-      for (int q = k; q < b; q++) {
-        string temp = "";
-        for (int d = k; d <= q; d++) {
-          temp += s[d];
-        }
-        sub_arr.push_back(temp);
-      }
-    }
-    int count = 0;
-    for (int i = 0; i < sub_arr.size(); i++) {
       int sum = 0;
-      for (char c : sub_arr[i]) {
-        sum += c - '0';
-      }
-      if (sum == sub_arr[i].size()) {
-        count += 1;
+      for (int q = k; q < b; q++) {
+        sum += s[q] - '0';
+        if (sum == q - k + 1) {
+          count += 1;
+        }
+        // string temp = "";
+        // for (int d = k; d <= q; d++) {
+        //   temp += s[d];
+        // }
+        // sub_arr.push_back(temp);
       }
     }
+    // int count = 0;
+    // for (int i = 0; i < sub_arr.size(); i++) {
+    //   int sum = 0;
+    //   for (char c : sub_arr[i]) {
+    //     sum += c - '0';
+    //   }
+    //   if (sum == sub_arr[i].size()) {
+    //     count += 1;
+    //   }
+    // }
     cout << count << endl;
   }
 }
